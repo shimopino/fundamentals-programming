3c173453ef1c134bdb75ca17b737d0273368615a
shimopino <shimoke4869@gmail.com>
2022-04-13 21:34:31 +0900

    feat: 第3章2節に関するサンプルコードを追加

---
+++ chapter3/exercise-3.2.ml
@@ -0,0 +1,11 @@
+let e = 2.7182 ;;
+(* val e : float = 2.7182 *)
+
+let positive = e > 0 ;;
+(* - : bool = true *)
+
+let seconds_of_day = 60 * 60 * 24 ;;
+(* val seconds_of_day : int = 86400 *)
+
+let name = "なんとか谷" ;;
+(* val name : string = "なんとか谷" *)
