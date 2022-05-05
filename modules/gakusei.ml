914bc31e28ec6852705c770d28ec3e0629f04397
shimopino <shimoke4869@gmail.com>
2022-05-05 13:04:08 +0900

    学生の型定義とサンプルデータを追加

---
+++ modules/gakusei.ml
@@ -0,0 +1,14 @@
+type gakusei_t = {
+  namae : string; (* 名前 *)
+  tensuu : int; (* 点数 *)
+  seiseki : string; (* 成績 *)
+}
+
+(* テスト *)
+let gakusei1 = { namae = "asai"; tensuu = 90; seiseki = "A" }
+let gakusei2 = { namae = "asai"; tensuu = 80; seiseki = "A" }
+let gakusei3 = { namae = "asai"; tensuu = 75; seiseki = "B" }
+let gakusei4 = { namae = "asai"; tensuu = 70; seiseki = "B" }
+let gakusei5 = { namae = "asai"; tensuu = 65; seiseki = "C" }
+let gakusei6 = { namae = "asai"; tensuu = 60; seiseki = "C" }
+let gakusei7 = { namae = "asai"; tensuu = 55; seiseki = "D" }
